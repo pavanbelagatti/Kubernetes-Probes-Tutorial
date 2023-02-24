8166d2d71139e770efcb3c75055eec3583898058
Pavan Belagatti <pavan@shippable.com>
2023-02-24 12:35:03 +0530

    First commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const notes = [{
-		noteId: 1,
-		noteContent: "Hey guys, add your important notes here."
-	}
+        noteId: 1,
+        noteContent: "Hey guys, add your important notes here."
+    }
 ]
 
 const app = express()
@@ -12,59 +12,64 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }))
 
 app.get("/", function (req, res) {
-	res.render("home", {
-		data: notes
-	})
+    res.render("home", {
+        data: notes
+    })
 })
 
 app.post("/", (req, res) => {
-	const noteContent = req.body.noteContent
-	const noteId = notes.length + 1;
+    const noteContent = req.body.noteContent
+    const noteId = notes.length + 1;
 
-	notes.push({
-		noteId: noteId,
-		noteContent: noteContent
-	})
+    notes.push({
+        noteId: noteId,
+        noteContent: noteContent
+    })
 
-	res.render("home", {
-		data: notes
-	})
+    res.render("home", {
+        data: notes
+    })
 })
 
 app.post('/update', (req, res) => {
-	var noteId = req.body.noteId;
-	var noteContent = req.body.noteContent;
-	
-	notes.forEach(note => {
-		if (note.noteId == noteId) {
-			note.noteContent = noteContent;
-		}
-	})
-	res.render("home", {
-		data: notes
-	})
+    var noteId = req.body.noteId;
+    var noteContent = req.body.noteContent;
+    
+    notes.forEach(note => {
+        if (note.noteId == noteId) {
+            note.noteContent = noteContent;
+        }
+    })
+    res.render("home", {
+        data: notes
+    })
 })
 
 app.post('/delete', (req, res) => {
-	var noteId = req.body.noteId;
+    var noteId = req.body.noteId;
 
-	var j = 0;
-	notes.forEach(note => {
-		j = j + 1;
-		if (note.noteId == noteId) {
-			notes.splice((j - 1), 1)
-		}
-	})
+    var j = 0;
+    notes.forEach(note => {
+        j = j + 1;
+        if (note.noteId == noteId) {
+            notes.splice((j - 1), 1)
+        }
+    })
 
-	res.render("home", {
-		data: notes
-	})
+    res.render("home", {
+        data: notes
+    })
 })
 
+// health check endpoint
+app.get('/healthz', function(req, res) {
+    res.status(200).send('ok');
+});
+
 app.listen(3000, (req, res) => {
-	console.log("App is running on port 3000")
+    console.log("App is running on port 3000")
 })
